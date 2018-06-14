04774f27c21da2075c1f23365fd94b0260f61f07
Taylor Kaldahl <tkaldahl90@gmail.com>
2018-06-14 16:18:50 -0400

    adjustments for Heroku

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/vocabDb')
+if (process.env.NODE_ENV === 'production') {
+    mongoose.connect(process.env.MLAB_URL)
+  } else {
+    mongoose.connect('mongodb://localhost/vocabDb')
+  }
+// mongoose.connect('mongodb://localhost/vocabDb')
 mongoose.Promise = Promise
 module.exports = mongoose
