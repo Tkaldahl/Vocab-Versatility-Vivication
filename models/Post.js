48a178f512a5d69903318c758e3ff38ca08936fb
Taylor Kaldahl <tkaldahl90@gmail.com>
2018-06-18 14:29:44 -0400

    small update to models

--- models/Post.js
+++ models/Post.js
@@ -6,7 +6,7 @@ const Post = new Schema({
   author: {type: Schema.ObjectId, ref: 'User'},
   public: {type: Boolean, default: false},
   createdAt: {type: Date, default: Date.now},
-  starterVocab: {type: Schema.ObjectId, ref: 'Vocab'}
+  vocabUsed: {type: Schema.ObjectId, ref: 'Vocab'}
 })
 
 module.exports = Post
